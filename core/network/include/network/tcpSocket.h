91d334ead3778fb3b6479704a894ec0d1def98e9
Terry <zhaterry@gmail.com>
2026-07-10 17:37:22 -0400

    initial CMAKE setup

---
+++ core/network/include/network/tcpSocket.h
@@ -0,0 +1,14 @@
+//
+// Created by Terry on 2026-07-10.
+//
+
+#ifndef TRDP_TCPSOCKET_H
+#define TRDP_TCPSOCKET_H
+
+namespace trdp::network {
+    class tcpSocket {
+    };
+}
+
+
+#endif //TRDP_TCPSOCKET_H
---
+++ core/network/src/tcpSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Terry on 2026-07-10.
+//
+
+#include "network/tcpSocket.h"
